fc98200ab1624e7c0652cd75a32dacf6ec628231
HaseebKahn365 <58126830+HaseebKahn365@users.noreply.github.com>
2023-08-02 21:25:53 +0500

    Added VideoController and more widgets.
    In this version of the clonetFlix, i added several responsive widgets in the Desktop mode.

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -4,6 +4,7 @@ import 'package:clonetflix/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 import '../models/content_model.dart';
+import 'package:video_player/video_player.dart';
 
 class ContentHeader extends StatelessWidget {
   final Content featuredContent;
@@ -79,64 +80,126 @@ class _ContentHeaderMobile extends StatelessWidget {
   }
 }
 
-class _ContentHeaderDesktop extends StatelessWidget {
+class _ContentHeaderDesktop extends StatefulWidget {
   final Content featuredContent;
 
   const _ContentHeaderDesktop({super.key, required this.featuredContent});
 
   @override
+  State<_ContentHeaderDesktop> createState() => _ContentHeaderDesktopState();
+}
+
+class _ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
+  VideoPlayerController? _videoController;
+  bool _isMuted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    final videoUri = Uri.parse(widget.featuredContent.videoUrl!);
+    _videoController = VideoPlayerController.network(videoUri.toString())
+      ..initialize().then((_) => setState(() {}))
+      ..setVolume(0)
+      ..play()
+      ..setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _videoController!.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          height: 500.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(featuredContent.imageUrl),
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => _videoController!.value.isPlaying ? _videoController!.pause() : _videoController!.play(),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          AspectRatio(
+              aspectRatio: _videoController!.value.isInitialized ? _videoController!.value.aspectRatio : 2.344,
+              child: _videoController!.value.isInitialized
+                  ? VideoPlayer(_videoController!)
+                  : Image.asset(
+                      widget.featuredContent.imageUrl,
+                      fit: BoxFit.cover,
+                    )),
+          Container(
+            height: 500.0,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.black, Colors.transparent],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
             ),
           ),
-        ),
-        Container(
-          height: 500.0,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.black, Colors.transparent],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
+          Positioned(
+            left: 60,
+            right: 60,
+            bottom: 150,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 250,
+                  child: Image.asset(widget.featuredContent.titleImageUrl!),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  widget.featuredContent.description!,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w100,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.25),
+                        offset: Offset(2.0, 4.0),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    _PlayButton(),
+                    const SizedBox(
+                      width: 16.0,
+                    ),
+                    VerticalIconButton(
+                      icon: Icons.info_outline,
+                      title: 'More Info',
+                      onTap: () {},
+                    ),
+                    const SizedBox(
+                      width: 20.0,
+                    ),
+                    if (_videoController!.value.isInitialized)
+                      IconButton(
+                        icon: Icon(
+                          _isMuted ? Icons.volume_off : Icons.volume_up,
+                        ),
+                        color: Colors.white,
+                        iconSize: 30.0,
+                        onPressed: () => setState(() {
+                          _videoController!.setVolume(_isMuted ? 100 : 0);
+                          _isMuted = _videoController!.value.volume == 0;
+                        }),
+                      )
+                  ],
+                )
+              ],
             ),
           ),
-        ),
-        Positioned(
-          bottom: 110.0,
-          child: SizedBox(
-            width: 250,
-            child: Image.asset(featuredContent.titleImageUrl!),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 40.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              VerticalIconButton(
-                icon: Icons.add,
-                title: 'List',
-                onTap: () {},
-              ),
-              _PlayButton(),
-              VerticalIconButton(
-                icon: Icons.info_outline,
-                title: 'Info',
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
